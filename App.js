f7efaded4d0086604523b4cd8df692fe8a8960b1
Pieroni <jpieroni10@gmail.com>
2023-06-09 19:19:57 -0300

    Atualizar para o Desafio 02

--- App.js
+++ App.js
@@ -1,45 +1,49 @@
 import React from 'react';
-
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Cadastro from './src/pages/Cadastro';
 import Login from './src/pages/Login';
+
+import TelaPrincipal from './src/pages/TelaPrincipal';
 
 const Stack = createNativeStackNavigator();
 
 export default function App(){
   return (
     <NavigationContainer>
-
       <Stack.Navigator>
-        
         <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{
-          title:"Tela de Login",
-          headerStyle: {
-            backgroundColor:"#AAA"
-          }
-        }}
-        >
-        </Stack.Screen>
-
+          name="TelaPrincipal"
+          component={TelaPrincipal}
+          options={{
+            title:"",
+            headerStyle: {
+              backgroundColor:"#AAA"
+            }
+          }}
+        />
         <Stack.Screen
-        name="Cadastro"
-        component={Cadastro}
-        options={{
-          title:"Tela de Cadastro",
-          headerStyle: {
-            backgroundColor:"#AAA"
-          }
-        }}
-        >
-        </Stack.Screen>
-
+          name="Login"
+          component={Login}
+          options={{
+            title:"",
+            headerStyle: {
+              backgroundColor:"#AAA"
+            }
+          }}
+        />
+        <Stack.Screen
+          name="Cadastro"
+          component={Cadastro}
+          options={{
+            title:"",
+            headerStyle: {
+              backgroundColor:"#AAA"
+            }
+          }}
+        />
       </Stack.Navigator>
-
     </NavigationContainer>
   );
 }
---
+++ src/pages/TelaPrincipal/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import Cadastro from '../Cadastro';
+import Login from '../Login';
+
+const Tab = createBottomTabNavigator();
+
+function TelaPrincipalScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo à Tela Principal</Text>
+    </View>
+  );
+}
+
+export default function PaginaPrincipal() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Tela Principal') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Login') {
+            iconName = 'log-in-outline';
+          } else if (route.name === 'Cadastro') {
+            iconName = 'person-add-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: '#f2f2f2',
+          borderTopWidth: 1,
+          borderTopColor: '#ccc',
+        },
+        tabBarLabelStyle: {
+          fontSize: 16,
+          fontWeight: 'bold',
+        },
+      })}
+    >
+      <Tab.Screen name="Tela Principal" component={TelaPrincipalScreen} />
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="Cadastro" component={Cadastro} />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal: 16,
+  },
+});
